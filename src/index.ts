2745b3d36a56768708d46666269be74b6dc2645e
pjp0704 <junhyeong25@gmail.com>
2021-10-11 23:23:42 +0900

    [ADD] cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
+import * as cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('common'));
 
 app.listen(process.env.PORT, () =>
