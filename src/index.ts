e362679c98d273959657230e18f6e5ff5b1bd2b6
pjp0704 <junhyeong25@gmail.com>
2021-10-11 22:25:25 +0900

    [ADD] morgan logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
 import 'dotenv/config';
 
 const app = express();
+
+app.use(morgan('common'));
 
 app.listen(process.env.PORT, () =>
   console.log('listening on port ' + process.env.PORT),
