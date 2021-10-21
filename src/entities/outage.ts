a826139d2395417949e8ef023e6a9388f60c63fc
pjp0704 <junhyeong25@gmail.com>
2021-10-22 00:57:27 +0900

    [ADD] Outage 엔티티

---
+++ src/entities/outage.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('outage')
+export class Outage {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  title!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  related!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: string;
+
+  @Column()
+  resolved!: boolean;
+}
