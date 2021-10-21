16bc6a573a802bd794269b78ec804c06da7d69f4
pjp0704 <junhyeong25@gmail.com>
2021-10-22 01:33:53 +0900

    [ADD] DB Connection Options

---
+++ src/shared/configs/databaseConnectionOptions.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { ConnectionOptions } from 'typeorm';
+
+export const connectionOptions: { [key: string]: ConnectionOptions } = {
+  development: {
+    type: 'postgres',
+    host: process.env.DEV_DB_HOST,
+    port: +process.env.DEV_DB_PORT,
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    synchronize: true,
+    logging: true,
+  },
+  production: {
+    type: 'postgres',
+    host: process.env.PROD_DB_HOST,
+    port: +process.env.PROD_DB_PORT,
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_DATABASE,
+    synchronize: false,
+    logging: false,
+  },
+};
