7aedbc2e9d86666841c22801c9353e22c755e9dd
pjp0704 <junhyeong25@gmail.com>
2021-10-22 12:50:18 +0900

    [CHANGE] PK to FK

--- src/shared/entities/comment.ts
+++ src/shared/entities/comment.ts
@@ -2,13 +2,19 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Outage } from './outage';
 
 @Entity()
 export class Comment {
-  @PrimaryGeneratedColumn()
-  id!: number;
+  @ManyToOne((type) => Outage, (outage) => outage.id, {
+    primary: true,
+  })
+  @JoinColumn({ name: 'outage_id' })
+  outageId!: Outage;
 
   @Column({ type: 'varchar', length: 20 })
   title!: string;
