6377ec4015439728f8a788e9bcb1799128b41c5a
pjp0704 <junhyeong25@gmail.com>
2021-10-25 01:26:06 +0900

    [ADD] swagger registration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import Fastify from 'fastify';
+import swagger from 'fastify-swagger';
 import DatabaseConnector from 'src/utils/decorators/databaseConnector';
 import { StatusController } from './controllers/status';
 
@@ -13,6 +14,28 @@ const fastify = Fastify({
 });
 
 fastify.register(DatabaseConnector);
+
+fastify.register(swagger, {
+  routePrefix: '/docs',
+  swagger: {
+    info: {
+      title: 'RMS Status backend',
+      description: 'Swagger document of RMS status backend',
+      version: process.env.npm_package_version!,
+    },
+    host: 'localhost',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [{ name: 'Status', description: '상태 정보 API' }],
+  },
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  staticCSP: true,
+  exposeRoute: true,
+});
 
 fastify.register(StatusController, { prefix: '/status' });
 
