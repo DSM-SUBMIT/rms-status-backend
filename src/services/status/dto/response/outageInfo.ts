202839005564ae1ceda5bd6ea80b69a21c677bf9
pjp0704 <junhyeong25@gmail.com>
2021-10-25 01:27:24 +0900

    [ADD] schemas

--- src/services/status/dto/response/outageInfo.ts
+++ src/services/status/dto/response/outageInfo.ts
@@ -11,3 +11,24 @@ export interface OutageInfo {
   severity: string;
   date: Date;
 }
+
+export const outageInfo = {
+  type: 'object',
+  properties: {
+    title: { type: 'string' },
+    posts: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          date: { type: 'string', example: '2021-10-23T21:40:44.248Z' },
+          content: { type: 'string' },
+        },
+      },
+    },
+    affected_on: { type: 'string' },
+    severity: { type: 'string', enum: ['yellow', 'red'] },
+    date: { type: 'string', example: '2021-10-23T21:40:44.248Z' },
+  },
+};
